9d23b61e0277bed27d6999353e889d497ac8975d
bhanujahuja <43927790+bhanujahuja@users.noreply.github.com>
2019-03-10 23:23:28 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## makeCacheMatrix function creates a "matrix" object that can cache its inverse,
+## then returns the same through get function, 
+## the matrix object can cache its own object,
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  makeCacheMatrix <- function(x = matrix()) { ## defining the argument with default "matrix"
 
 }
 
